b9f4874e6cfc8be6272e4a59f3b85d8d523631b2
fi944 <132758667+fi944@users.noreply.github.com>
2025-08-02 22:53:44 +0300

    Update config.py

--- Auto-Forward-Bot-V2-main/config.py
+++ Auto-Forward-Bot-V2-main/config.py
@@ -1,13 +1,13 @@
 from os import environ 
 
 class Config:
-    API_ID = environ.get("API_ID", "577678")
-    API_HASH = environ.get("API_HASH", "d2c6e01uuiuiouioiuiou0fc6d7a1be")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "70955...") 
+    API_ID = environ.get("API_ID", "10122221")
+    API_HASH = environ.get("API_HASH", "0599f028e8b0e5ac86dc36cbe32d87d8")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7594886219:AAE0-iPwo1y38QTeRMkocxcGCt-v_9B2ksM") 
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://chhjgjkkjhkjhkjh@cluster0.xowzpr4.mongodb.net/")
     DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6964148334').split()]
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '881535564').split()]
 
 class temp(object): 
     lock = {}
